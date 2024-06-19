eead88a328544c42d586903e7fdb7a921a2f3ac6
JulioNa <janavarrete8@espe.edu.ec>
2024-06-18 22:32:47 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { IndexComponent } from './index/index.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'index',
+    pathMatch: 'full'
+  },
+  {
+    path: 'index',
+    component: IndexComponent,
+  },
+  {
+    path: 'catalogo',
+    component: CatalogoComponent,
+  },
+  {
+    path: 'formulario',
+    component: FormularioComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
