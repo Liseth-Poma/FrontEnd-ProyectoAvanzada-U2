82a4462711e92287600ad99d14f5090cbcb0b076
Jair Sánchez <jssanchez9@espe.edu.ec>
2024-06-18 23:24:40 -0500

    hola

--- src/app/catalogo/catalogo.component.ts
+++ src/app/catalogo/catalogo.component.ts
@@ -14,17 +14,17 @@ export class CatalogoComponent {
     {
       name: 'The Last of Us',
       description: 'Un juego de supervivencia en un mundo post-apocalíptico',
-      image: 'https://example.com/tlou.jpg'
+      image: 'https://uvejuegos.com/img/caratulas/46761/TLOU%20Francia.jpg'
     },
     {
       name: 'God of War',
       description: 'Un juego de acción y aventuras en un mundo mitológico',
-      image: 'https://example.com/gow.jpg'
+      image: 'https://ecuadorjuegosdigitales.com/wp-content/uploads/2022/10/1643412757-god-of-war-ragnarok-ps4-pre-orden.jpg'
     },
     {
       name: 'Horizon Zero Dawn',
       description: 'Un juego de acción y aventuras en un mundo post-apocalíptico',
-      image: 'https://example.com/hzd.jpg'
+      image: 'https://cdn1.epicgames.com/3328b08ac1c14540aa265a1a85c07839/offer/hzd_tall-1200x1600-d4b1057afd47f9256d8da71f2f187be4.jpg'
     },
     // ...
   ];
