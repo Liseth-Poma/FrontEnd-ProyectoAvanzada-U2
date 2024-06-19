555da875ccfc18bca1cdf97877081567dda36a01
JulioNa <janavarrete8@espe.edu.ec>
2024-06-18 23:00:26 -0500

    first commit

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './index.component.html',
-  styleUrl: './index.component.css'
+  styleUrls: ['./index.component.css']
 })
 export class IndexComponent {
+  constructor(private router: Router) {
+  }
+  featuredGames = [
+    {
+      image: 'https://example.com/game1.jpg',
+      name: 'The Last of Us',
+      description: 'Un juego de supervivencia en un mundo post-apocalíptico'
+    },
+    {
+      image: 'https://example.com/game2.jpg',
+      name: 'God of War',
+      description: 'Un juego de acción y aventura en un mundo mitológico'
+    },
+    // ...
+  ];
+  goToCatalogo() {
+    // Redirigir a la página de catálogo
+    this.router.navigate(['/catalogo']);
+  }
 
+  goToFormulario() {
+    // Redirigir a la página de formulario
+    this.router.navigate(['/formulario']);
+  }
 }
