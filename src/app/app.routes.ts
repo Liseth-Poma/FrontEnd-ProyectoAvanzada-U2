8e3fcd994c8363c77d96fdd95029f0c265d03c4e
Liseth-Poma <lcpoma@espe.edu.ec>
2024-06-18 22:53:16 -0500

    Formulario-Registo-Compra

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { VideojuegoFormComponent } from './videojuego-form/videojuego-form.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'Formulario-Compra',
+        component: VideojuegoFormComponent,
+      },
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+  })
+  export class AppRoutingModule {}
---
+++ src/app/videojuego-form/videojuego-form.component.ts
@@ -0,0 +1,100 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RecaptchaFormsModule, RecaptchaModule } from 'ng-recaptcha';
+import Swal from 'sweetalert2';
+
+
+@Component({
+    selector: 'app-videojuego-form',
+    standalone: true,
+    providers: [FormBuilder],
+    templateUrl: './videojuego-form.component.html',
+    styleUrl: './videojuego-form.component.css',
+    imports: [ReactiveFormsModule, FormsModule, CommonModule, RecaptchaModule, RecaptchaFormsModule]
+})
+export class VideojuegoFormComponent implements OnInit{
+
+  form: FormGroup;
+  precios: { [key: string]: number } = {
+    'Resident Evil 4': 50,
+    'The Last of Us': 60,
+    'Minecraft': 70,
+    'Soulmask': 80,
+    'Mario Kart 8': 90,
+    'Resident Evil 2': 50,
+    'The Last of Us 1': 60,
+    'Minecraft t': 70,
+    'Soulmask 4': 80,
+    'Mario Kart 8 2': 90
+  };
+  captchaValid: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nombre: ['', [Validators.required, Validators.pattern('^[a-zA-Z]{3,15}$')]],
+      correo: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@gmail\\.com$'), Validators.minLength(10), Validators.maxLength(50)]],
+      telefono: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      videojuego: ['', Validators.required],
+      precio: [{ value: '', disabled: true }, Validators.required],
+      codigo: [''],
+      subtotal: [{ value: '', disabled: true }, Validators.required],
+      total: [{ value: '', disabled: true }, Validators.required],
+      
+    });
+  }
+
+  ngOnInit(): void {
+    this.form.get('videojuego')?.valueChanges.subscribe(value => {
+      if (value) {
+        const precio = this.precios[value];
+        this.form.get('precio')?.setValue(precio);
+        this.actualizarTotal();
+      }
+    });
+
+    this.form.get('codigo')?.valueChanges.subscribe(() => {
+      this.actualizarTotal();
+    });
+  }
+
+  actualizarTotal(): void {
+    const precio = this.form.get('precio')?.value;
+    let subtotal = precio;
+    const codigo = this.form.get('codigo')?.value;
+
+    if (codigo && codigo !== 'Avanzada') {
+      this.form.get('codigo')?.setErrors({ invalidCode: true });
+    } else {
+      if (codigo === 'Avanzada') {
+        subtotal *= 0.9; // 10% de descuento
+        this.form.get('codigo')?.setErrors(null); // Eliminar errores si el código es válido
+      }
+      this.form.get('subtotal')?.setValue(subtotal);
+      this.form.get('total')?.setValue(subtotal);
+    }
+  }
+
+  resolved(captchaResponse: string | null) {
+    this.captchaValid = captchaResponse !== null && captchaResponse.length > 0;
+    if (this.captchaValid) {
+      this.form.get('captcha')?.setValue(captchaResponse);
+    } else {
+      this.form.get('captcha')?.setValue(null);
+    }
+  }
+
+  onSubmit(): void {
+    if (this.form.valid && this.captchaValid) {
+      Swal.fire('Formulario enviado', 'Revise su correo', 'success');
+      this.form.reset();
+      // Mantener los campos deshabilitados después del reset
+      this.form.get('precio')?.disable();
+      this.form.get('subtotal')?.disable();
+      this.form.get('total')?.disable();
+    
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+}
